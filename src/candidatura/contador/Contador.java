583aa3d4c1a59ac2f420b9087dd6e81a1dd7a21c
Euriks27 <128837824+Euriks27@users.noreply.github.com>
2024-06-23 04:08:41 -0400

    Update Contador.java

--- src/candidatura/contador/Contador.java
+++ src/candidatura/contador/Contador.java
@@ -1,27 +1,99 @@
-package candidatura.contador;
+package candidatura;
 
-import java.util.Scanner;
+import java.util.Random;
 
-public class Contador {
-	public static void main(String[] args) {
-		Scanner terminal = new Scanner(System.in);
-		System.out.println("Digite o primeiro parâmetro");
-		int parametroUm = terminal.nextInt();
-		System.out.println("Digite o segundo parâmetro");
-		int parametroDois = terminal.nextInt();
-		
-		try {
-			//chamando o método contendo a lógica de contagem
-			contar(parametroUm, parametroDois);
-		
-		}catch (ParametrosInvalidosException exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
-            System.out.println("O segundo parametro deve ser maior que o primeiro");
-		}
-		
-	}
-	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+public class ProcessoSeletivo {
+    public static void main(String[] args) {
+        System.out.println("Processo seletivo");
+
+        analisarCandidato(1400.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+
+        System.out.println("\n-- Seleção de candidatos --");
+        selecaoCandidatos();
+
+        System.out.println("\n-- Imprimir Selecionados --");
+        imprimirSelecionados();
+
+        String[] candidatos = {"Maria", "João", "Ana", "Pedro", "Clara", "Lucas", "Isabella", "Bruno", "Carolina", "Gustavo"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        } else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRAPROPOSTA");
+        } else {
+            System.out.println("AGUARDANDO DEMAIS CANDIDATOS");
+        }
+    }
+
+    static void selecaoCandidatos() {
+        String[] candidatos = {"Maria", "João", "Ana", "Pedro", "Clara", "Lucas", "Isabella", "Bruno", "Carolina", "Gustavo"};
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("Candidato: " + candidato + " - valor: " + salarioPretendido);
+
+            if (salarioBase >= salarioPretendido) {
+                System.out.println(candidato + " selecionado para a vaga!");
+                candidatosSelecionados++;
+            }
+
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1500, 2500);
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = {"Maria", "João", "Ana", "Pedro", "Clara", "Lucas", "Isabella", "Bruno", "Carolina", "Gustavo"};
+        System.out.println("Imprimindo a lista de candidatos de acordo com o índice");
+
+        for (int i = 0; i < candidatos.length; i++) {
+            System.out.println("O candidato de número " + (i + 1) + " é " + candidatos[i]);
+        }
+    }
+
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuaTentando = true;
+        boolean atendeu = false;
+
+        do {
+            atendeu = atender();
+            continuaTentando = !atendeu;
+            if (continuaTentando) {
+                tentativasRealizadas++;
+            } else {
+                System.out.println("Contato realizado com sucesso!");
+            }
+        } while (continuaTentando && tentativasRealizadas < 3);
+
+        if (atendeu) {
+            System.out.println("Conseguimos o contato com " + candidato + " com " + tentativasRealizadas + " tentativas!");
+        } else {
+            System.out.println("NÃO conseguimos o contato com " + candidato + " com " + tentativasRealizadas + " tentativas!");
+        }
+    }
+}
+
         if(parametroUm > parametroDois) {
             throw new ParametrosInvalidosException();
         }
